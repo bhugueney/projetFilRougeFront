65ce3318350777b1dcfae8d496b469826f151a6a
Frédérick PAREJA <Frederick@MacBook-Pro-de-Frederick.local>
2018-06-11 17:18:10 +0200

    add categorie.model

--- src/app/models/ingredient.model.ts
+++ src/app/models/ingredient.model.ts
@@ -1,2 +1,23 @@
+import { Categorie } from './categorie.model';
 export class Ingredient {
+  private id: number;
+  private name: string;
+
+  private energy: number;
+  private water: number;
+  private protein: number;
+  private glucid: number;
+  private lipid: number;
+  private sugar: number;
+  private amidon: number;
+  private fiber: number;
+  private unsaturedFattyAcides: number;
+  private monoUnsaturedFattyAcides: number;
+  private polyUnsaturedFattyAcides: number;
+  private salt: number;
+
+  private categorie: Categorie;
+
+  private owner;
+
 }
