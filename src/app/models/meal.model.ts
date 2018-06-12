1c822dca0a8a6eae81e9e0f7dbcb3f4d020b7b59
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-06-12 10:39:10 +0200

    Create Meal Class  getters and setters

--- src/app/models/meal.model.ts
+++ src/app/models/meal.model.ts
@@ -2,6 +2,23 @@ import { MealType } from './meal-type';
 import { Preparation } from './preparation.model';
 
 export class Meal extends Preparation {
-    timeStamp: Date;
-    mealType: MealType;
+    private timeStamp: Date;
+    private mealType: MealType;
+
+
+    public getTimeStamp(): Date {
+        return this.timeStamp;
+    }
+
+    public setTimeStamp(timeStamp: Date) {
+        this.timeStamp = timeStamp;
+    }
+
+    public getMealType(): MealType {
+        return this.mealType;
+    }
+
+    public setMealType(mealType: MealType) {
+        this.mealType = mealType;
+    }
 }
