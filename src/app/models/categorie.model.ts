c13f12c4569f318ec7e357d95c63fc1208d5b5bc
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-06-12 12:40:28 +0200

    Getters and setters user et categorie

--- src/app/models/categorie.model.ts
+++ src/app/models/categorie.model.ts
@@ -1,5 +1,27 @@
 export class Categorie {
-  private id: number;
-  private name: string;
-  private parent: Categorie;
+  private _id: number;
+  private _name: string;
+  private _parent: Categorie;
+
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+
+  public get parent(): Categorie {
+    return this._parent;
+  }
+  public set parent(value: Categorie) {
+    this._parent = value;
+  }
+
 }
--- src/app/services/preparation.service.ts
+++ src/app/services/preparation.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../models/ingredient.model';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,59 @@ import { Injectable } from '@angular/core';
 })
 export class PreparationService {
 
-  constructor() { }
+   ingredients: Ingredient[];
+
+  constructor() {
+
+    // Add somme ingrédients to mock
+    this.ingredients.push(Object.assign(new Ingredient(), JSON.parse(`
+    {
+      _id: 1,
+      _name: 'Carrotes'
+      '_urlImage': 'carrotes.img',
+      '_energy': 50,
+      '_water': 10,
+      '_protein': 30,
+      '_glucid': 20,
+      '_lipid': 30,
+      '_sugar': 25,
+      '_amidon': 10,
+      '_fiber': 10,
+      '_unsaturedFattyAcides': 30,
+      '_monoUnsaturedFattyAcides': 20,
+      '_polyUnsaturedFattyAcides': 50,
+      '_salt': 5,
+      '_categorie': {_id: 1, _name: 'Fruits'},
+      '_owner': {'_id': 0, '_firstName': '', '_lastName': 'system', '_email': '' },
+      '_comment': 'ceci n\'est pas une carrotte'
+     }
+   `)));
+
+   this.ingredients.push(Object.assign(new Ingredient(), JSON.parse(`
+   {
+     _id: 2,
+     _name: 'Tomates'
+     '_urlImage': 'tomates.img',
+     '_energy': 50,
+     '_water': 10,
+     '_protein': 30,
+     '_glucid': 20,
+     '_lipid': 30,
+     '_sugar': 25,
+     '_amidon': 10,
+     '_fiber': 10,
+     '_unsaturedFattyAcides': 30,
+     '_monoUnsaturedFattyAcides': 20,
+     '_polyUnsaturedFattyAcides': 50,
+     '_salt': 5,
+     '_categorie': {_id: 1, _name: 'Fruits'},
+     '_owner': {'_id': 0, '_firstName': '', '_lastName': 'system', '_email': '' },
+     '_comment': 'ceci n\'est pas une tomate'
+    }
+  `)));
+
+
+
+
+   }
 }
