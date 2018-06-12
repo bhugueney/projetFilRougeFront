61ff1164d8e38a0b634a8e06c54ce54007659db4
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-06-12 10:13:28 +0200

    Add mealType and define meal class model

--- src/app/models/meal.model.ts
+++ src/app/models/meal.model.ts
@@ -1,4 +1,8 @@
+import { MealType } from './meal-type';
 import { Preparation } from './preparation.model';
 
 export class Meal extends Preparation {
+    timeStamp: Date;
+    mealType: MealType;
+    comment: String;
 }
