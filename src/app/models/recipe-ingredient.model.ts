d18d05dc516f67d7fc7ed04f14c12bc3e18d90a0
gabrielW <gabriel.wisniewski@gmail.com>
2018-06-14 14:19:33 +0200

    Modification of Recipe to handle RecipeIngredient list instead of Ingredient list

--- src/app/models/recipe-ingredient.model.ts
+++ src/app/models/recipe-ingredient.model.ts
@@ -2,8 +2,29 @@ import { Ingredient } from './ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeIngredient {
-    private quantity: number;
-    private ingredient: Ingredient;
-    private recipe: Recipe;
+    private _quantity: number;
+    private _ingredient: Ingredient;
+    private _recipe: Recipe;
+
+    public get recipe(): Recipe {
+        return this._recipe;
+    }
+    public set recipe(value: Recipe) {
+        this._recipe = value;
+    }
+
+    public get quantity(): number {
+        return this._quantity;
+    }
+    public set quantity(value: number) {
+        this._quantity = value;
+    }
+
+    public get ingredient(): Ingredient {
+        return this._ingredient;
+    }
+    public set ingredient(value: Ingredient) {
+        this._ingredient = value;
+    }
 
 }
--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,30 +1,34 @@
 import { Ingredient } from './ingredient.model';
+import { RecipeIngredient } from './recipe-ingredient.model';
 
 export class Recipe extends Ingredient {
   // content of the recipe : since the Ingredient class
-  private _listIngredient: Ingredient[];
-  public get listIngredient(): Ingredient[] {
+  private _listIngredient: RecipeIngredient[];
+  public get listIngredient(): RecipeIngredient[] {
     return this._listIngredient;
   }
-  public set listIngredient(value: Ingredient[]) {
+  public set listIngredient(value: RecipeIngredient[]) {
     this._listIngredient = value;
   }
 
   // method to cumulate data informations of every ingredient
   private dataCalcul() {
-    this.listIngredient.forEach(element => {
-      this.energy += element.energy;
-      this.water += element.water;
-      this.protein += element.protein;
-      this.glucid += element.glucid;
-      this.lipid += element.lipid;
-      this.sugar += element.sugar;
-      this.amidon += element.amidon;
-      this.fiber += element.fiber;
-      this.unsaturedFattyAcides += element.unsaturedFattyAcides;
-      this.monoUnsaturedFattyAcides += element.monoUnsaturedFattyAcides;
-      this.polyUnsaturedFattyAcides += element.polyUnsaturedFattyAcides;
-      this.salt += element.salt;
-    });
+    this.listIngredient.forEach(
+      recipeIngredient => {
+        const element: Ingredient = recipeIngredient.ingredient;
+
+        this.energy += element.energy;
+        this.water += element.water;
+        this.protein += element.protein;
+        this.glucid += element.glucid;
+        this.lipid += element.lipid;
+        this.sugar += element.sugar;
+        this.amidon += element.amidon;
+        this.fiber += element.fiber;
+        this.unsaturedFattyAcides += element.unsaturedFattyAcides;
+        this.monoUnsaturedFattyAcides += element.monoUnsaturedFattyAcides;
+        this.polyUnsaturedFattyAcides += element.polyUnsaturedFattyAcides;
+        this.salt += element.salt;
+      });
   }
 }
