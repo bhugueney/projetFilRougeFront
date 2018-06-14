c131ce041f926f28e11ec0f2dbf783eaf3714c09
gabrielW <gabriel.wisniewski@gmail.com>
2018-06-14 17:55:34 +0200

    Add test of ingredient type to prepare display of ingredient list

--- src/app/components/ingredient/ingredient.component.ts
+++ src/app/components/ingredient/ingredient.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../models/ingredient.model';
 import { IngredientService } from '../../services/ingredient.service';
+import { Recipe } from '../../models/recipe.model';
+import { RecipeService } from '../../services/recipe.service';
+import { TypeCheckCompiler } from '@angular/compiler/src/view_compiler/type_check_compiler';
+import { Meal } from '../../models/meal.model';
 
 @Component({
   selector: 'app-ingredient',
@@ -11,10 +15,31 @@ export class IngredientComponent implements OnInit {
 
   ingredient: Ingredient;
 
-  constructor(private ingredientService: IngredientService) { }
+  recette: Recipe;
+
+  constructor(private recipeService: RecipeService,
+    private ingredientService: IngredientService,
+  ) { }
 
   ngOnInit() {
     this.ingredient = this.ingredientService.getCarotte();
+    // this.ingredient = this.recipeService.getRecipeById(1);
+
+    // This command allow to get name of real class
+    switch (this.ingredient.constructor.name) {
+      case Recipe.name:
+        console.log('this.ingredient is a recipe');
+        break;
+      case Ingredient.name:
+        console.log('this.ingredient is a basic ingredient');
+        break;
+      case Meal.name:
+        console.log('this.ingredient is a meal');
+        break;
+    }
+
+    console.log('type of ingredient : ' + typeof (this.ingredient));
+
   }
 
 }
