a75251aec3baf8fcc68db77ad1e14575f704554a
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-06-15 12:39:14 +0200

    Ajout affichage data sur préparation component et mise au point calcul datas

--- src/app/components/preparation/preparation.component.ts
+++ src/app/components/preparation/preparation.component.ts
@@ -16,9 +16,35 @@ export class PreparationComponent implements OnInit {
 
   ngOnInit() {
     this.preparation = this.recipeService.getById(0);
+    this.preparation.dataCalcul();
   }
 
   public deleteRecipeIngredient(recipeIngredientPosition: number) {
+    console.log('Position : ' + recipeIngredientPosition);
     this.preparation.listIngredient.splice(recipeIngredientPosition, 1);
+    this.preparation.dataCalcul();
+  }
+
+
+  public increaseQty(recipeIngredientPosition: number): void {
+    this.preparation.listIngredient[recipeIngredientPosition].quantity += 10;
+    this.preparation.dataCalcul();
+  }
+
+  public decreaseQty(recipeIngredientPosition: number): void {
+    this.preparation.listIngredient[+recipeIngredientPosition].quantity -= 10;
+    if (this.preparation.listIngredient[+recipeIngredientPosition].quantity < 0.0) {
+      this.preparation.listIngredient[+recipeIngredientPosition].quantity = 0.0;
+    }
+    this.preparation.dataCalcul();
+  }
+
+
+  public updateQty(recipeIngredientPosition: number, Qty: number) {
+    this.preparation.listIngredient[+recipeIngredientPosition].quantity = +Qty;
+    if (this.preparation.listIngredient[+recipeIngredientPosition].quantity < 0.0) {
+      this.preparation.listIngredient[+recipeIngredientPosition].quantity = 0.0;
+    }
+    this.preparation.dataCalcul();
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -26,19 +26,19 @@ export class RecipeService {
     recipeIngredient1.id = 1;
     recipeIngredient1.recipe = fakeRecipe;
     recipeIngredient1.ingredient = this.ingredientService.getById(1);
-    recipeIngredient1.quantity = 1.5;
+    recipeIngredient1.quantity = 200.0;
 
     const recipeIngredient2: RecipeIngredient = new RecipeIngredient();
     recipeIngredient1.id = 2;
     recipeIngredient2.recipe = fakeRecipe;
     recipeIngredient2.ingredient = this.ingredientService.getById(2);
-    recipeIngredient2.quantity = 1.0;
+    recipeIngredient2.quantity = 300.0;
 
     const recipeIngredient3: RecipeIngredient = new RecipeIngredient();
     recipeIngredient1.id = 3;
     recipeIngredient3.recipe = fakeRecipe;
     recipeIngredient3.ingredient = this.ingredientService.getById(3);
-    recipeIngredient3.quantity = 2.5;
+    recipeIngredient3.quantity = 50;
 
     const listIngredient: RecipeIngredient[] = new Array<RecipeIngredient>();
     listIngredient.push(recipeIngredient1);
@@ -46,6 +46,21 @@ export class RecipeService {
     listIngredient.push(recipeIngredient3);
 
     fakeRecipe.listIngredient = listIngredient;
+    fakeRecipe.id = 0;
+    fakeRecipe.name = '(Nouvelle préparation)';
+    fakeRecipe.energy = 0.0;
+    fakeRecipe.water = 0.0;
+    fakeRecipe.protein = 0.0;
+    fakeRecipe.glucid = 0.0;
+    fakeRecipe.lipid = 0.0;
+    fakeRecipe.sugar = 0.0;
+    fakeRecipe.amidon = 0.0;
+    fakeRecipe.fiber = 0.0;
+    fakeRecipe.unsaturedFattyAcides = 0.0;
+    fakeRecipe.monoUnsaturedFattyAcides = 0.0;
+    fakeRecipe.polyUnsaturedFattyAcides = 0.0;
+    fakeRecipe.salt = 0.0;
+
 
     return fakeRecipe;
 
