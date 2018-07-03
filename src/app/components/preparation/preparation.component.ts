b654dee795bb6ad3be48a62b1da8b49b2f942ac7
HPoumeyrol <hugues.poumeyrol.pf1@gmail.com>
2018-07-03 10:59:49 +0200

    comit synchro retour

--- src/app/components/preparation/preparation.component.ts
+++ src/app/components/preparation/preparation.component.ts
@@ -1,3 +1,4 @@
+import { RecipeIngredient } from './../../models/recipe-ingredient.model';
 import { Ingredient } from './../../models/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
@@ -11,40 +12,45 @@ import { Recipe } from '../../models/recipe.model';
 export class PreparationComponent implements OnInit {
 
   preparation: Recipe;
+  ingredientDetails: Ingredient;
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.preparation = this.recipeService.getById(0);
+    this.ingredientDetails = this.preparation.listIngredient[0].ingredient;
     this.preparation.dataCalcul();
   }
 
-  public deleteRecipeIngredient(recipeIngredientPosition: number) {
-    console.log('Position : ' + recipeIngredientPosition);
-    this.preparation.listIngredient.splice(recipeIngredientPosition, 1);
+  public deleteRecipeIngredient(recipeIngredient: RecipeIngredient) {
+    this.recipeService.DeleteRecipeIngredient(this.preparation, recipeIngredient);
     this.preparation.dataCalcul();
   }
 
 
-  public increaseQty(recipeIngredientPosition: number): void {
-    this.preparation.listIngredient[recipeIngredientPosition].quantity += 10;
+  public increaseQty(recipeIngredient: RecipeIngredient): void {
+    recipeIngredient.quantity += 10;
     this.preparation.dataCalcul();
   }
 
-  public decreaseQty(recipeIngredientPosition: number): void {
-    this.preparation.listIngredient[+recipeIngredientPosition].quantity -= 10;
-    if (this.preparation.listIngredient[+recipeIngredientPosition].quantity < 0.0) {
-      this.preparation.listIngredient[+recipeIngredientPosition].quantity = 0.0;
+  public decreaseQty(recipeIngredient: RecipeIngredient): void {
+    recipeIngredient.quantity -= 10;
+    if (recipeIngredient.quantity < 0.0) {
+      recipeIngredient.quantity = 0.0;
     }
     this.preparation.dataCalcul();
   }
 
 
-  public updateQty(recipeIngredientPosition: number, Qty: number) {
-    this.preparation.listIngredient[+recipeIngredientPosition].quantity = +Qty;
-    if (this.preparation.listIngredient[+recipeIngredientPosition].quantity < 0.0) {
-      this.preparation.listIngredient[+recipeIngredientPosition].quantity = 0.0;
+  public updateQty(recipeIngredient: RecipeIngredient, Qty: number) {
+    recipeIngredient.quantity = +Qty;
+    if (recipeIngredient.quantity < 0.0) {
+      recipeIngredient.quantity = 0.0;
     }
     this.preparation.dataCalcul();
+  }
+
+  public showDetails(ingredientToShow: Ingredient) {
+    this.ingredientDetails = ingredientToShow;
   }
 }
